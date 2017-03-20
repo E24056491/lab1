66a21195b4922eaf10c90ad2430a396d1d137cc7
E24056491 <treelikes3@gmail.com>
2017-03-21 00:28:12 +0800

    add lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	
+	cin >> n;
+	cout << n << endl;
+	
+	while(n != 1)
+	{
+ 
+     if(n%2 == 1)
+	  {
+	     n = n*3 + 1;
+	  }
+	  else
+	  {
+		n = n/2;
+	  }
+	  cout<< n << endl;
+ 	}
+}	
+	
